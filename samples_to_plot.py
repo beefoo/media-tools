5da2e6428f7b027d3543325fb16f27f969284236
brianfoo <brian@youaremyjoy.org>
2018-12-02 16:08:03 -0500

    allow to plot arbirtrary properties

--- samples_to_plot.py
+++ samples_to_plot.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 
+# python -W ignore samples_to_plot.py -in tmp/ia_politicaladarchive_samples.csv -props "power,hz" -log 1 -lim -1
+
 import argparse
 import inspect
+import math
 from matplotlib import pyplot as plt
 import os
 from pprint import pprint
 import sys
-
-# add parent directory to sys path to import relative modules
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir)
 
 from lib.collection_utils import *
 from lib.io_utils import *
@@ -19,16 +17,20 @@ from lib.math_utils import *
 # input
 parser = argparse.ArgumentParser()
 parser.add_argument('-in', dest="INPUT_FILE", default="tmp/samples_tsne.csv", help="Input file")
+parser.add_argument('-props', dest="PROPS", default="tsne,tsne2", help="X and Y properties")
 parser.add_argument('-sort', dest="SORT", default="flatness=desc=0.5&power=desc", help="Query string to sort by")
 parser.add_argument('-lim', dest="LIMIT", default=1296, type=int, help="Target total sample count, -1 for everything")
+parser.add_argument('-log', dest="LOG", default=0, type=int, help="Display using log?")
 parser.add_argument('-highlight', dest="HIGHLIGHT", default="", help="Property to highlight")
 args = parser.parse_args()
 
 # Parse arguments
 INPUT_FILE = args.INPUT_FILE
+PROP1, PROP2 = tuple([p for p in args.PROPS.strip().split(",")])
 SORT = args.SORT
 LIMIT = args.LIMIT
 HIGHLIGHT = args.HIGHLIGHT
+LOG = args.LOG
 
 # Read files
 fieldNames, rows = readCsv(INPUT_FILE)
@@ -40,9 +42,14 @@ rows = sortByQueryString(rows, SORT)
 if LIMIT > 0 and len(rows) > LIMIT:
     rows = rows[:LIMIT]
 
-x = [row["tsne"] for row in rows]
-y = [row["tsne2"] for row in rows]
-
+x = [row[PROP1] for row in rows]
+y = [row[PROP2] for row in rows]
+if LOG > 1:
+    x = [math.log(row[PROP1], LOG) for row in rows]
+    y = [math.log(row[PROP2], LOG) for row in rows]
+elif LOG > 0:
+    x = [math.log(row[PROP1]) for row in rows]
+    y = [math.log(row[PROP2]) for row in rows]
 
 plt.figure(figsize = (10,10))
 if len(HIGHLIGHT) > 0:
