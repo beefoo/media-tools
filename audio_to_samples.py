058ad4117422a04898095949ecfd67d8382463a8
brianfoo <brian@youaremyjoy.org>
2019-01-10 10:27:59 -0500

    audio to samples: fix check for existing samples

--- audio_to_samples.py
+++ audio_to_samples.py
@@ -123,9 +123,10 @@ if FEATURES:
 totalCount = 0
 for i, f in enumerate(files):
     fn = f["filename"]
+    basename = os.path.basename(fn)
 
     # Check if we already have this data
-    if not OVERWRITE and rowCount > 0 and len([row for row in rows if row["filename"]==fn]) > 0:
+    if not OVERWRITE and rowCount > 0 and len([row for row in rows if row["filename"]==basename]) > 0:
         totalCount += len([row for row in rows if row["filename"]==fn])
         print("Already found samples for %s. Skipping.")
 
