c228e057a756ad134b0e374e7d9fb4d813ae6ade
Brian Foo <brian@youaremyjoy.org>
2019-01-22 16:33:47 -0500

    modify sample stats to be more general

--- stats.py
+++ stats.py
@@ -15,6 +15,7 @@ parser.add_argument('-in', dest="INPUT_FILE", default="tmp/samples.csv", help="I
 parser.add_argument('-bins', dest="BINS", type=int, default=50, help="Bins for histogram")
 parser.add_argument('-ncols', dest="NCOLS", type=int, default=3, help="Number of columns")
 parser.add_argument('-fig', dest="FIG_SIZE", default=" 16,8", help="Figure size in inches")
+parser.add_argument('-plot', dest="PLOT_KEYS", default="power,dur,clarity,hz,note,octave", help="Comma separated list of parameters to plot")
 args = parser.parse_args()
 
 # Parse arguments
@@ -22,6 +23,7 @@ INPUT_FILE = args.INPUT_FILE
 BINS = args.BINS
 NCOLS = args.NCOLS
 FIG_SIZE = tuple([int(d) for d in args.FIG_SIZE.strip().split(",")])
+PLOT_KEYS = args.PLOT_KEYS.strip().split(",")
 
 # Read files
 rows = []
@@ -29,34 +31,25 @@ fieldNames, rows = readCsv(INPUT_FILE)
 rowCount = len(rows)
 print("Found %s rows" % rowCount)
 
-plotConfig = [
-    {"key": "power", "title": "Power distribution"},
-    {"key": "dur", "title": "Duration distribution"},
-    {"key": "clarity", "title": "Clarity distribution"},
-    {"key": "hz", "title": "Frequency distribution"},
-    {"key": "note", "title": "Note distribution"},
-    {"key": "octave", "title": "Octave distribution"}
-]
-propertyCount = len(plotConfig)
-nrows = ceilInt(1.0 * propertyCount / NCOLS)
+propertyCount = len(PLOT_KEYS)
+ncols = NCOLS if propertyCount > NCOLS else propertyCount
+nrows = ceilInt(1.0 * propertyCount / ncols)
 
 plt.figure(figsize=FIG_SIZE)
 
-number = 1
-for i, p in enumerate(plotConfig):
-    bins = BINS if "bins" not in p else p["bins"]
-    data = [parseFloat(r[p["key"]]) for r in rows if p["key"] in r]
+for i, key in enumerate(PLOT_KEYS):
+    data = [parseFloat(r[key]) for r in rows if p[key] in r]
     if len(data) > 0:
         first = data[0]
-        ax = plt.subplot(nrows, NCOLS, i+1)
-        ax.set_title(p["title"])
+        ax = plt.subplot(nrows, ncols, i+1)
+        ax.set_title(key)
         if isNumber(first):
-            plt.hist(data, bins=bins)
+            plt.hist(data, bins=BINS)
         else:
             labels = unique(data)
             ndata = [labels.index(d) for d in data]
             plt.xticks(range(len(labels)), labels)
-            plt.hist(ndata, bins=bins)
+            plt.hist(ndata, bins=BINS)
 
 plt.tight_layout()
 plt.show()
