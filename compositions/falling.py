3d95b62757071220c08257cf748d5df06343526b
beefoo <brian@youaremyjoy.org>
2019-03-17 14:34:16 -0400

    falling: reduce grid cycles

--- compositions/falling.py
+++ compositions/falling.py
@@ -34,7 +34,7 @@ parser.add_argument('-grid0', dest="START_GRID", default="128x128", help="Start 
 parser.add_argument('-grid1', dest="END_GRID", default="32x32", help="End size of grid")
 parser.add_argument('-maxcd', dest="MAX_COLUMN_DELTA", default=16, type=int, help="Max number of columns to move left and right")
 parser.add_argument('-waves', dest="WAVE_COUNT", default=6, type=int, help="Number of sine waves to do")
-parser.add_argument('-gridc', dest="GRID_CYCLES", default=4, type=int, help="Number of times to go through the full grid")
+parser.add_argument('-gridc', dest="GRID_CYCLES", default=3, type=int, help="Number of times to go through the full grid")
 parser.add_argument('-duration', dest="TARGET_DURATION", default=120, type=int, help="Target duration in seconds")
 parser.add_argument('-translate', dest="TRANSLATE_AMOUNT", default=0.5, type=float, help="Amount to translate clip as a percentage of height")
 parser.add_argument('-prad', dest="PLAY_RADIUS", default=8.0, type=float, help="Radius of cells/clips to play at any given time")
