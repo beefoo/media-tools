0d7581c0e77cabafa17370d6da786a913ece1a44
Brian Foo <brian@youaremyjoy.org>
2019-03-22 17:46:39 -0400

    wind comp: implement basic movement logic

--- compositions/wind.py
+++ compositions/wind.py
@@ -34,12 +34,15 @@ parser.add_argument('-grid0', dest="START_GRID", default="64x64", help="Start si
 parser.add_argument('-grid1', dest="END_GRID", default="128x128", help="End size of grid")
 parser.add_argument('-volr', dest="VOLUME_RANGE", default="0.3,0.6", help="Volume range")
 parser.add_argument('-dur', dest="DURATION_MS", default=60000, type=int, help="Target duration in seconds")
-parser.add_argument('-windd', dest="WIND_DURATION_MS", default=6000, type=int, help="Wind duration in seconds")
-parser.add_argument('-bounds', dest="DATA_BOUNDS", default="0.0,0.0,1.0,1.0", help="Bounds x,y,w,h as a percentage")
+parser.add_argument('-dmax', dest="DATA_MAX", default=8.0, type=float, help="Intended data max (absolute range is typically around -20 to 20); lower this to move things faster")
+parser.add_argument('-smax', dest="SPEED_MAX", default=2.0, type=float, help="Most we should move a clip per frame in pixels; assumes 30fps; assumes 1920x1080px")
 a = parser.parse_args()
 parseVideoArgs(a)
 aa = vars(a)
-aa["DATA_BOUNDS"] = tuple([float(v) for v in a.DATA_BOUNDS.strip().split(",")])
+# adjust speed max for this fps and resolution
+aa["SPEED_MAX"] = a.SPEED_MAX * (a.WIDTH / 1920.0) / (a.FPS / 30.0)
+aa["PAD_END"] = 12000
+aa["THREADS"] = 1 # enforce one thread since we need to process frames sequentially
 
 # Get video data
 startTime = logTime()
@@ -56,6 +59,10 @@ stepTime = logTime(stepTime, "Samples to clips")
 for i, clip in enumerate(clips):
     clip.vector.setParent(container.vector)
 
+fromScale = 1.0 * gridW / startGridW
+toScale = 1.0 * gridW / endGridW
+container.queueTween(a.PAD_START, a.DURATION_MS, ("scale", fromScale, toScale, "cubicInOut"))
+
 dataFiles = getFilenames(a.WIND_DATA)
 windData = []
 for fn in dataFiles:
@@ -70,6 +77,28 @@ print("Wind data shape = %s x %s x %s x %s" % windData.shape)
 print("Wind data shape = %s x %s x %s x %s" % windData.shape)
 print("Wind range [%s, %s]" % (windData.min(), windData.max()))
 stepTime = logTime(stepTime, "Normalize data")
+
+# slice the data: take the middle half
+steps, h, w, uv = windData.shape
+percent = 0.5
+sw = roundInt(w * percent)
+sh = roundInt(sw / (1.0 * a.WIDTH / a.HEIGHT))
+sx = roundInt((w - sw) * 0.5)
+sy = roundInt((h - sh) * 0.5)
+slicedWindData = np.zeros((steps, sh, sw, uv), dtype=windData.dtype)
+for i in range(steps):
+    slicedWindData[i] = windData[i,sy:(sy+sh),sx:(sx+sw),:]
+windData = slicedWindData
+print("Wind data shape after slice = %s x %s x %s x %s" % windData.shape)
+
+# Initialize clip states
+for i, clip in enumerate(clips):
+    clip.setState("lastPlayedMs", 0)
+    clip.setState("pos", (clip.props["x"], clip.props["y"]))
+    clip.setState("rotation", 0)
+    lifeDuration = clip.dur * 16
+    clip.setState("lifeDuration", lifeDuration)
+    clip.setState("life", lifeDuration)
 
 # Write wind data to image
 # u = windData[0][:,:,0]
@@ -88,14 +117,88 @@ stepTime = logTime(stepTime, "Normalize data")
 # plt.show()
 # sys.exit()
 
-ms = a.PAD_START + a.DURATION_MS + a.WIND_DURATION_MS
+startMs = a.PAD_START
+ms = startMs + a.DURATION_MS
+endMs = ms
 
 # sort frames
 container.vector.sortFrames()
 
+stepData = None
+currentMs = -1
+
+# zero degrees = 3 o'clock (east), increasing clockwise
+def angleFromUV(u, v):
+    return 360.0 - math.atan2(v, u) * 180.0 / math.pi % 360.0
+
 # custom clip to numpy array function to override default tweening logic
 def clipToNpArrWind(clip, ms, containerW, containerH, precision, parent):
+    global a
+    global windData
+    global startMs
+    global endMs
+    global stepData
+    global currentMs
+
+    dsteps, dh, dw, _ = windData.shape
+
+    frameMs = frameToMs(1, a.FPS)
+    life = clip.getState("life")
+    lifeDuration = clip.getState("lifeDuration")
+    alpha = clip.props["alpha"]
+    alphaMultiplier = 0.0 if life < 0 else (1.0 * life / lifeDuration)
+    alpha *= alphaMultiplier
     customProps = None
+
+    if alpha > 0:
+        nprogress = norm(ms, (startMs, endMs))
+        fDataIndex = 1.0 * nprogress * (dsteps-1)
+        dataIndex0 = floorInt(fDataIndex)
+        dataIndex1 = ceilInt(fDataIndex)
+
+        # ensure we only do this once per frame and not for every clip per frame
+        if ms > currentMs or stepData is None:
+            currentMs = ms
+            # we're before the start
+            if nprogress < 0.0:
+                stepData = windData[0]
+            # we're after the end
+            elif nprogress > 1.0:
+                stepData = windData[-1]
+            # lerp data if we're in-between data frames
+            elif dataIndex1 > dataIndex0:
+                stepData = lerp((windData[dataIndex0], windData[dataIndex1]), nprogress)
+            else:
+                stepData = windData[dataIndex0]
+
+        # increase in speed over time; fastest in the middle
+        globalSpeed = easeSinInOutBell(lim(nprogress))
+
+        x, y = clip.getState("pos")
+        nx, ny = (1.0 * x / a.WIDTH, 1.0 * y / a.HEIGHT)
+        nx, ny = (lim(nx), lim(ny))
+        dcol, drow = (roundInt((dw-1) * nx), roundInt((dh-1) * ny))
+        u, v = tuple(stepData[drow, dcol])
+        nu, nv = (u / a.DATA_MAX, v / a.DATA_MAX)
+        nu, nv = (lim(nu, (-1.0, 1.0)), lim(nv, (-1.0, 1.0)))
+        moveX, moveY = (nu * a.SPEED_MAX * globalSpeed, nv * a.SPEED_MAX * globalSpeed)
+
+        x += moveX
+        y += moveY
+        clip.setState("pos", (x, y))
+        customProps = {
+            "pos": [x, y]
+        }
+
+    if ms > startMs:
+        life -= frameMs
+
+    # reset life if died and we still have time for another full life
+    if life <= 0 and ms < endMs-lifeDuration:
+        life = lifeDuration
+        clip.setState("pos", (clip.props["x"], clip.props["y"]))
+    clip.setState("life", life)
+
     precisionMultiplier = int(10 ** precision)
     props = clip.toDict(ms, containerW, containerH, parent, customProps=customProps)
 
@@ -104,9 +207,11 @@ def clipToNpArrWind(clip, ms, containerW, containerH, precision, parent):
         roundInt(props["y"] * precisionMultiplier),
         roundInt(props["width"] * precisionMultiplier),
         roundInt(props["height"] * precisionMultiplier),
-        roundInt(props["alpha"] * precisionMultiplier),
+        roundInt(alpha * precisionMultiplier),
         roundInt(props["tn"] * precisionMultiplier),
-        roundInt(props["zindex"])
+        roundInt(props["zindex"]),
+        roundInt(props["rotation"] * precisionMultiplier),
+        roundInt(props["blur"] * precisionMultiplier)
     ], dtype=np.int32)
 
-processComposition(a, clips, ms, sampler, stepTime, startTime, customClipToArrFunction=clipToNpArrFalling)
+processComposition(a, clips, ms, sampler, stepTime, startTime, customClipToArrFunction=clipToNpArrWind, containsAlphaClips=True, isSequential=True)
