466e8af6c402ccfed2ead5811ce671a746ab343e
beefoo <brian@youaremyjoy.org>
2019-04-05 15:41:30 -0400

    update gpu order test

--- tests/gpuOrder.py
+++ tests/gpuOrder.py
@@ -14,7 +14,6 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
-from lib.clip import *
 from lib.gpu_utils import *
 from lib.io_utils import *
 from lib.math_utils import *
@@ -35,30 +34,38 @@ colors = [
     (0, 0, 255),
     (255, 255, 0)
 ]
-clips = []
 clipCount = len(colors) * 4
-properties = np.zeros((clipCount, 5), dtype=np.int32)
-clipPixelCount = CLIP_W * CLIP_H * 3
-pixelData = np.zeros(clipPixelCount * clipCount, dtype=np.uint8)
-offset = 0
 margin = ((HEIGHT * 0.5) - CLIP_H) * 0.5 / len(colors)
-for i, c in enumerate(colors):
-    for j in range(4):
-        marginIX = (len(colors)-i) if j % 2 > 0 else i+1
-        marginIY = (len(colors)-i) if j < 2 else i+1
-        x = (j % 2) * (WIDTH * 0.5) + margin * marginIX
-        y = margin * marginIY if j < 2 else (HEIGHT * 0.5) + margin * marginIY
-        im = Image.new(mode="RGB", size=(CLIP_W, CLIP_H), color=c)
-        pixels = np.array(im, dtype=np.uint8)
-        pixelData[offset:(offset+clipPixelCount)] = pixels.reshape(-1)
-        properties[i*4+j] = np.array([offset, roundInt(x), roundInt(y), CLIP_W, CLIP_H])
-        offset += clipPixelCount
+clipPixelCount = CLIP_W * CLIP_H * 3
+for step in range(5):
+    properties = np.zeros((clipCount, 5), dtype=np.int32)
+    pixelData = np.zeros(clipPixelCount * clipCount, dtype=np.uint8)
+    offset = 0
+    for i, c in enumerate(colors):
+        for j in range(4):
+            marginIX = (len(colors)-i) if j % 2 > 0 else i+1
+            marginIY = (len(colors)-i) if j < 2 else i+1
+            if step == 0:
+                marginIX = i+1
+                marginIY = i+1
+            elif step == 1:
+                marginIX = (len(colors)-i)
+                marginIY = (len(colors)-i)
+            elif step == 2:
+                marginIX = i+1
+            elif step == 3:
+                marginIY = i+1
+            x = (j % 2) * (WIDTH * 0.5) + margin * marginIX
+            y = margin * marginIY if j < 2 else (HEIGHT * 0.5) + margin * marginIY
+            im = Image.new(mode="RGB", size=(CLIP_W, CLIP_H), color=c)
+            pixels = np.array(im, dtype=np.uint8)
+            pixelData[offset:(offset+clipPixelCount)] = pixels.reshape(-1)
+            properties[i*4+j] = np.array([offset, roundInt(x), roundInt(y), CLIP_W, CLIP_H])
+            offset += clipPixelCount
 
-for i in range(8):
     pixels = clipsToImageGPULite(WIDTH, HEIGHT, pixelData, properties)
     im = Image.fromarray(pixels, mode="RGB")
-    filename = OUTPUT_FILE % (i+1)
+    filename = OUTPUT_FILE % (step+1)
     im.save(filename)
     print("Saved %s" % filename)
-
 print("Done.")
