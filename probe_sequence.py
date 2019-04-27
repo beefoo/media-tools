4bfe783fc2b269c0ee5eb6362e7550101896f1ca
beefoo <brian@youaremyjoy.org>
2019-04-27 13:37:44 -0400

    add probe sequence

---
+++ probe_sequence.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# python3 sort_audio.py -overwrite -pyv python3 -in media/sample/LivingSt1958.mp4
+
+import argparse
+import os
+import subprocess
+import sys
+
+from lib.math_utils import *
+
+# input
+parser = argparse.ArgumentParser()
+parser.add_argument('-in', dest="INPUT_FILE", default="tmp/ia_fedflixnara_subset_128x128.csv", help="Input sample csv file")
+parser.add_argument('-seq', dest="SEQUENCE", default="proliferation,waves,falling,orbits,shuffle,stretch", help="Comma separated list of compositions")
+parser.add_argument('-pyv', dest="PYTHON_NAME", default="python3", help="Name of python command")
+a = parser.parse_args()
+
+SEQUENCE = a.SEQUENCE.strip().split(",")
+
+runningTotalMs = 0
+for comp in SEQUENCE:
+    command = [a.PYTHON_NAME, 'compositions/%s.py' % comp, '-in', a.INPUT_FILE, '-probe']
+    print(" ".join(command))
+    result = subprocess.check_output(command).strip()
+    lines = [line.strip() for line in result.splitlines()]
+    lastLine = lines[-1].decode("utf-8")
+    compMs = int(lastLine.split(":")[-1].strip())
+    runningTotalMs += compMs
+    print("Running total: %s (%s)" % (runningTotalMs, formatSeconds(runningTotalMs/1000.0)))
+    print("------")
