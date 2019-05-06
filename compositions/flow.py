2fb14d0f323b448a4d965406b36e0c00647ce1fe
beefoo <brian@youaremyjoy.org>
2019-05-06 00:39:17 -0400

    flow comp: highlight circle

--- compositions/flow.py
+++ compositions/flow.py
@@ -35,10 +35,10 @@ parser.add_argument('-grid0', dest="START_GRID", default="128x128", help="Start 
 parser.add_argument('-grid1', dest="END_GRID", default="128x128", help="End size of grid")
 parser.add_argument('-volr', dest="VOLUME_RANGE", default="0.3,0.8", help="Volume range")
 parser.add_argument('-radius', dest="RADIUS", default=4.0, type=float, help="Target radius as a percentage of clip height")
-parser.add_argument('-freq', dest="FREQ_RANGE", default="2.0,4.0", help="Frequency range")
+parser.add_argument('-freq', dest="FREQ_RANGE", default="2.0,2.0", help="Frequency range")
 parser.add_argument('-rdur', dest="ROTATION_DUR", default=8000, type=int, help="Target duration in ms")
 parser.add_argument('-rot', dest="ROTATIONS", default=8, type=int, help="Total number of rotations")
-parser.add_argument('-tdur', dest="TRANSITION_DUR", default=8000, type=int, help="How long it takes to transition to/from target frequency and radius")
+
 a = parser.parse_args()
 parseVideoArgs(a)
 aa = vars(a)
@@ -59,12 +59,16 @@ for i, s in enumerate(samples):
 clips = samplesToClips(samples)
 stepTime = logTime(stepTime, "Samples to clips")
 
+degreesPerMs = 360.0 / a.ROTATION_DUR
 for i, clip in enumerate(clips):
     clip.vector.setParent(container.vector)
     clip.setState("cx", clip.props["x"] + clip.props["width"] * 0.5)
     clip.setState("cy", clip.props["y"] + clip.props["height"] * 0.5)
     rotationOffset = 1.0 * ((gridW-clip.props["row"]-1)+clip.props["col"]) / (gridH + gridW - 2) * 2.0 * math.pi
     clip.setState("rotationOffset", rotationOffset)
+    nBrightness = norm(clip.props["distanceFromCenter"], (0.5, min(gridW*0.5, gridH*0.5)-0.5), limit=True)
+    nBrightness = easeSinInOutBell(nBrightness)
+    clip.setState("nBrightness", nBrightness)
 
 fromScale = 1.0 * gridW / startGridW
 toScale = 1.0 * gridW / endGridW
@@ -88,32 +92,24 @@ def clipToNpArrFlow(clip, ms, containerW, containerH, precision, parent, globalA
     global endMs
 
     customProps = None
+    brightness = a.BRIGHTNESS_RANGE[0]
 
     if startMs <= ms < endMs:
 
-        transitionInMs = startMs + a.TRANSITION_DUR
-        transitionOutMs = endMs - a.TRANSITION_DUR
-        radius = a.RADIUS
-        freq = a.FREQ_RANGE[1]
-        nprogress = 1.0
-
-        # Check if we're transitioning
-        if ms < transitionInMs:
-            nprogress = norm(ms, (startMs, transitionInMs))
-        elif ms > transitionOutMs:
-            nprogress = 1.0 - norm(ms, (transitionOutMs, endMs))
-        if nprogress < 1.0:
-            nprogress = ease(nprogress)
-            radius = a.RADIUS * nprogress
-            freq = lerp(a.FREQ_RANGE, nprogress)
-
-        radius *= clip.props["height"]
+        nprogress = norm(ms, (startMs, endMs))
+        nprogress = easeSinInOutBell(nprogress)
+        radius = a.RADIUS * nprogress * clip.props["height"]
+        freq = lerp(a.FREQ_RANGE, nprogress)
 
         # Rotate clip around its center
         nrotation = 1.0 * (ms % a.ROTATION_DUR) / a.ROTATION_DUR
         angle = (1.0 - nrotation) * 2.0 * math.pi + clip.getState("rotationOffset") * freq
         cx, cy = translatePoint(clip.getState("cx"), clip.getState("cy"), radius, angle, radians=True)
         x, y = (cx - clip.props["width"]*0.5, cy - clip.props["height"]*0.5)
+
+        # Set brightness
+        brightness = lerp(a.BRIGHTNESS_RANGE, nprogress * clip.getState("nBrightness"))
+
         customProps = {
             "pos": [x, y]
         }
@@ -131,7 +127,7 @@ def clipToNpArrFlow(clip, ms, containerW, containerH, precision, parent, globalA
         roundInt(props["zindex"]),
         roundInt(props["rotation"] * precisionMultiplier),
         roundInt(props["blur"] * precisionMultiplier),
-        roundInt(props["brightness"] * precisionMultiplier)
+        roundInt(brightness * precisionMultiplier)
     ], dtype=np.int32)
 
 processComposition(a, clips, durationMs, sampler, stepTime, startTime, customClipToArrFunction=clipToNpArrFlow, containsAlphaClips=True, isSequential=True)
