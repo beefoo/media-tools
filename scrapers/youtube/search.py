52b0cb9ba948c69aac754dfb77031ce0540313a3
Brian Foo <brian@youaremyjoy.org>
2019-07-08 17:27:14 -0400

    youtube search: implement output and pagination

--- scrapers/youtube/search.py
+++ scrapers/youtube/search.py
@@ -21,31 +21,35 @@ sys.path.insert(0,parentdir)
 
 from lib.collection_utils import *
 from lib.io_utils import *
+from lib.math_utils import *
 
 # input
 parser = argparse.ArgumentParser()
 parser.add_argument('-key', dest="API_KEY", default="", help="Your API Key. See: https://google-developers.appspot.com/youtube/v3/getting-started")
-parser.add_argument('-in', dest="QUERY_FILE", default="", help="CSV file with queries")
-parser.add_argument('-query', dest="QUERY", default=" location=45.517967,-73.732461&locationRadius=10km&videoLicense=creativeCommon", help="Search query parameters as a query string")
+parser.add_argument('-query', dest="QUERY", default=" location=40.903125,-73.85062&locationRadius=10km&videoLicense=creativeCommon", help="Search query parameters as a query string")
 parser.add_argument('-sort', dest="SORT_BY", default="", help="Sort by string")
-parser.add_argument('-lim', dest="LIMIT", default=50, type=int, help="Limit results")
-parser.add_argument('-out', dest="OUTPUT_FILE", default="", help="CSV output file to save results")
+parser.add_argument('-lim', dest="LIMIT", default=100, type=int, help="Limit results")
+parser.add_argument('-out', dest="OUTPUT_FILE", default="tmp/yt-search/%s.json", help="JSON output file pattern")
+parser.add_argument('-verbose', dest="VERBOSE", action="store_true", help="Display search result details")
 a = parser.parse_args()
+aa = vars(a)
+makeDirectories([a.OUTPUT_FILE])
 
-QUERY = a.QUERY.strip()
+aa["QUERY"] = a.QUERY.strip()
+MAX_YT_RESULTS_PER_PAGE = 50
 
 if len(a.API_KEY) <= 0:
     print("You must pass in your developer API key. See more at https://google-developers.appspot.com/youtube/v3/getting-started")
     sys.exit()
 
-if len(QUERY) <= 0:
+if len(a.QUERY) <= 0:
     print("Please pass in a query.")
 
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=a.API_KEY)
 
-ytQuery = parseQueryString(QUERY)
+ytQuery = parseQueryString(a.QUERY)
 
 ytQuery["part"] = "id,snippet"
 ytQuery["type"] = "video" # Always get videos back
@@ -54,26 +58,53 @@ ytQuery["videoDimension"] = "2d" # exclude 3d videos
 if len(a.SORT_BY) > 0:
     ytQuery["order"] = a.SORT_BY
 
+pages = 1
 if a.LIMIT > 0:
-    ytQuery["maxResults"] = a.LIMIT
+    pages = ceilInt(1.0 * a.LIMIT / MAX_YT_RESULTS_PER_PAGE)
+    ytQuery["maxResults"] = min(a.LIMIT, MAX_YT_RESULTS_PER_PAGE)
 
-# Make one query to retrieve ids
-search_response = youtube.search().list(**ytQuery).execute()
-ids = []
-for r in search_response.get('items', []):
-    ids.append(r['id']['videoId'])
+results = []
+for page in range(pages):
+    print("Page %s..." % (page+1))
+    # Make one query to retrieve ids
+    search_response = youtube.search().list(**ytQuery).execute()
+    nextPageToken = search_response.get('nextPageToken', "")
+    # pprint(search_response.get('items', []))
+    # sys.exit()
 
-# Make another query to retrieve stats
-idString = ",".join(ids)
-search_response = youtube.videos().list(id=idString, part="id,statistics,snippet").execute()
-print("-----\nResults: ")
-for r in search_response.get('items', []):
-    # pprint(r['id'])
-    # pprint(r['statistics'])
-    # pprint(r['snippet'])
-    print("%s: %s (%s views)" % (r['id'], r['snippet']['title'], r['statistics']['viewCount']))
-print("-----")
+    ids = []
+    for r in search_response.get('items', []):
+        ids.append(r['id']['videoId'])
+    print("%s results found." % (len(ids)))
 
-# writeCsv(a.OUTPUT_FILE, results, fieldNames)
+    missingIds = []
+    for id in ids:
+        outfile = a.OUTPUT_FILE % id
+        if not os.path.isfile(outfile):
+            missingIds.append(id)
+
+    if len(missingIds) > 0:
+        print("Getting details for %s videos..." % (len(missingIds)))
+        # Make another query to retrieve stats
+        idString = ",".join(ids)
+        search_response = youtube.videos().list(id=idString, part="id,statistics,snippet").execute()
+        if a.VERBOSE:
+            print("-----\nResults: ")
+        for r in search_response.get('items', []):
+            outfile = a.OUTPUT_FILE % r['id']
+            writeJSON(outfile, r, verbose=a.VERBOSE)
+            # pprint(r['id'])
+            # pprint(r['statistics'])
+            # pprint(r['snippet'])
+            if a.VERBOSE:
+                print("%s: %s (%s views)" % (r['id'], r['snippet']['title'], r['statistics']['viewCount']))
+        if a.VERBOSE:
+            print("-----")
+
+    # Retrieve the next page
+    if len(nextPageToken) < 1:
+        break
+
+    ytQuery["pageToken"] = nextPageToken
 
 print("Done.")
