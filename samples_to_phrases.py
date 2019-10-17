28fa01eafbca0301a84debb3c7d0b5790b08c9d0
beefoo <brian@youaremyjoy.org>
2019-10-16 21:47:55 -0400

    output samples to phrases

--- samples_to_phrases.py
+++ samples_to_phrases.py
@@ -16,7 +16,7 @@ from lib.math_utils import *
 parser = argparse.ArgumentParser()
 parser.add_argument('-in', dest="INPUT_FILE", default="tmp/samples.csv", help="Input file")
 parser.add_argument('-min', dest="MIN_DUR", default=4, type=float, help="Minumum phrase duration in seconds")
-parser.add_argument('-max', dest="MAX_DUR", default=-1, type=float, help="Maximum phrase duration in seconds")
+parser.add_argument('-max', dest="MAX_DUR", default=30, type=float, help="Maximum phrase duration in seconds")
 parser.add_argument('-mins', dest="MIN_SAMPLES", default=8, type=int, help="At least this many samples per phrase")
 parser.add_argument('-msd', dest="MAX_SAMPLE_DUR", default=5, type=float, help="Maximum sample duration in seconds")
 parser.add_argument('-clarity', dest="CLARITY_THRESHOLD", default=0.4, type=float, help="Mean should be above this clarity")
@@ -26,6 +26,7 @@ parser.add_argument('-maxc', dest="MIN_PHRASE_CLARITY", default=30.0, type=float
 parser.add_argument('-maxp', dest="MAX_PHRASES", default=-1, type=int, help="Maximum phrases to retrieve; -1 for all")
 parser.add_argument('-maxs', dest="MAX_SAMPLES", default=-1, type=int, help="Maximum samples to retrieve; -1 for all")
 parser.add_argument('-out', dest="OUTPUT_FILE", default="output/phrases.csv", help="Output csv file")
+parser.add_argument('-probe', dest="PROBE", action="store_true", help="Just show details?")
 a = parser.parse_args()
 aa = vars(a)
 
@@ -39,6 +40,11 @@ fieldNames, samples = readCsv(a.INPUT_FILE)
 # samples = addIndices(samples)
 samples = addNormalizedValues(samples, "clarity", "nclarity")
 samples = addNormalizedValues(samples, "power", "npower")
+
+def getPhraseDur(samples):
+    start = samples[0]["start"]
+    dur = samples[-1]["start"] + samples[-1]["dur"] - start
+    return (start, dur)
 
 def getPhraseFeatures(samples, clarityKey="clarity", powerKey="power"):
     # weights = [s["dur"] for s in samples]
@@ -68,19 +74,19 @@ def addPhrase(phrases, phrase):
     if len(phrase) < a.MIN_SAMPLES:
         return phrases
 
-    start = phrase[0]["start"]
-    dur = phrase[-1]["start"] + phrase[-1]["dur"] - start
-    maxDur = a.MAX_DUR if a.MAX_DUR > 0 else dur
+    start, dur = getPhraseDur(phrase)
     # check for valid duration
-    if a.MIN_DUR <= dur <= maxDur:
+    if a.MIN_DUR <= dur:
         clarity, power = getPhraseFeatures(phrase)
+        mdur = np.median([s["dur"] for s in phrase])
         phrases.append({
             "filename": phrase[0]["filename"],
             "start": start,
             "dur": dur,
             "count": len(phrase),
-            "clarity": clarity,
-            "power": power,
+            "clarity": round(clarity, 3),
+            "power": round(power, 3),
+            "medianDur": roundInt(mdur),
             "samples": phrase
         })
 
@@ -101,6 +107,12 @@ for i, s in enumerate(samples):
         if s["nclarity"] >= a.CLARITY_THRESHOLD and s["npower"] >= a.POWER_THRESHOLD:
             currentPhrase.append(s)
         continue
+
+    # check if current phrase is too long
+    start, dur = getPhraseDur(currentPhrase)
+    if dur >= a.MAX_DUR:
+        phrases = addPhrase(phrases, currentPhrase)
+        currentPhrase = []
 
     # add sample to current phrase
     currentPhrase.append(s)
@@ -149,10 +161,17 @@ if a.MAX_SAMPLES > 0:
 
 phrases = addIndices(phrases, "rank", 1)
 phrases = sorted(phrases, key=lambda s: s["start"])
-
 print("Found %s valid phrases" % len(phrases))
 
-runningTotal = 0
-for i, phrase in enumerate(phrases):
-    runningTotal += phrase["count"]
-    print("%s. %s -> %s dur[%s] count[%s] clarity[%s] power[%s] total[%s] rank[%s]" % (i+1, formatSeconds(phrase["start"]/1000.0), formatSeconds(phrase["start"]/1000.0+phrase["dur"]/1000.0), formatSeconds(phrase["dur"]/1000.0), phrase["count"], round(phrase["clarity"], 2), round(phrase["power"], 2), runningTotal, phrase["rank"]))
+if a.PROBE:
+    runningTotal = 0
+    runningDur = 0
+    for i, phrase in enumerate(phrases):
+        runningTotal += phrase["count"]
+        runningDur += phrase["dur"]
+        print("%s. %s -> %s dur[%s] count[%s] clarity[%s] power[%s] total[%s] running[%s] rank[%s]" % (i+1, formatSeconds(phrase["start"]/1000.0), formatSeconds(phrase["start"]/1000.0+phrase["dur"]/1000.0), formatSeconds(phrase["dur"]/1000.0), phrase["count"], round(phrase["clarity"], 2), round(phrase["power"], 2), runningTotal, formatSeconds(runningDur/1000.0), phrase["rank"]))
+    sys.exit()
+
+headings = ["filename", "start", "dur", "count", "clarity", "power", "medianDur"]
+writeCsv(a.OUTPUT_FILE, phrases, headings=headings)
+print("Done.")
