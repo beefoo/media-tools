abdf7f7b070883bdabca4c953e1f798e12023d68
beefoo <brian@youaremyjoy.org>
2019-10-23 00:38:00 -0400

    add support for getting features from a directory of files and output metadata file

--- get_file_features.py
+++ get_file_features.py
@@ -10,26 +10,26 @@ import sys
 
 # input
 parser = argparse.ArgumentParser()
-parser.add_argument('-in', dest="INPUT_FILE", default="tmp/ia_politicaladarchive.csv", help="Input file")
+parser.add_argument('-in', dest="INPUT_FILE", default="tmp/ia_politicaladarchive.csv", help="Input file pattern")
 parser.add_argument('-dir', dest="MEDIA_DIRECTORY", default="media/downloads/ia_politicaladarchive/", help="Input dir")
-parser.add_argument('-fk', dest="FILENAME_KEY", default="filename", help="Key to retrieve filename")
 parser.add_argument('-dk', dest="DURATION_KEY", default="duration", help="Key for duration")
 parser.add_argument('-fast', dest="FAST_NOT_ACCURATE", action="store_true", help="Check duration accurately by opening it? (takes longer)")
+parser.add_argument('-out', dest="OUTPUT_FILE", default="", help="Output csv file; leave blank if same as input")
 parser.add_argument('-overwrite', dest="OVERWRITE", action="store_true", help="Overwrite existing non-zero durations?")
 a = parser.parse_args()
 
 # Parse arguments
 INPUT_FILE = a.INPUT_FILE
-MEDIA_DIRECTORY = a.MEDIA_DIRECTORY
-FILENAME_KEY = a.FILENAME_KEY
+OUTPUT_FILE = a.OUTPUT_FILE if len(a.OUTPUT_FILE) > 0 else INPUT_FILE
 ACCURATE = (not a.FAST_NOT_ACCURATE)
 
 keysToAdd = [a.DURATION_KEY, "hasAudio", "hasVideo"]
 
+makeDirectories(OUTPUT_FILE)
+
 # get unique video files
-print("Reading file...")
-fieldNames, rows = readCsv(INPUT_FILE)
-rowCount = len(rows)
+print("Reading files...")
+fieldNames, rows, rowCount = getFilesFromString(a)
 print("Found %s rows" % rowCount)
 
 # add keys
@@ -42,15 +42,16 @@ for i, row in enumerate(rows):
     duration = row[a.DURATION_KEY] if a.DURATION_KEY in row else 0
     hasAudio = row["hasAudio"] if "hasAudio" in row else 0
     hasVideo = row["hasVideo"] if "hasVideo" in row else 0
-    if FILENAME_KEY in row and len(row[FILENAME_KEY]) > 0 and (duration <= 0 or duration == "" or a.OVERWRITE):
-        filepath = MEDIA_DIRECTORY + row[FILENAME_KEY]
-        types = getMediaTypes(filepath)
+    if duration <= 0 or duration == "" or a.OVERWRITE:
+        types = getMediaTypes(row["filename"])
         hasAudio = 1 if "audio" in types else 0
         hasVideo = 1 if "video" in types else 0
-        if hasAudio and not hasVideo:
-            duration = getDurationFromAudioFile(filepath)
-        else:
-            duration = getDurationFromFile(filepath, ACCURATE)
+        if hasAudio > 0 or hasVideo > 0:
+            if hasAudio > 0 and hasVideo < 1:
+                duration = getDurationFromAudioFile(row["filename"])
+            else:
+                duration = getDurationFromFile(row["filename"], ACCURATE)
+    rows[i]["filename"] = os.path.basename(row["filename"])
     rows[i][a.DURATION_KEY] = duration
     rows[i]["hasAudio"] = hasAudio
     rows[i]["hasVideo"] = hasVideo
