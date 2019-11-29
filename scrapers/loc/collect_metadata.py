e61fb8a30248b4c8c08630c96254e36432185181
beefoo <brian@youaremyjoy.org>
2019-11-29 14:08:19 -0500

    loc collect metadata: change contributor to contributors

--- scrapers/loc/collect_metadata.py
+++ scrapers/loc/collect_metadata.py
@@ -26,18 +26,30 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-in', dest="INPUT_FILES", default="output/loc/pd_audio/items/*.json", help="File pattern for json files downloaded from download_metadata.py")
 parser.add_argument('-out', dest="OUTPUT_FILE", default="tmp/loc/lc_pd_audio.csv", help="File to write data to")
 parser.add_argument('-probe', dest="PROBE", action="store_true", help="Just print details?")
+parser.add_argument('-overwrite', dest="OVERWRITE", action="store_true", help="Overwrite existing data?")
 parser.add_argument('-threads', dest="THREADS", type=int, default=4, help="How many concurrent requests?")
 a = parser.parse_args()
 
 filenames = getFilenames(a.INPUT_FILES)
+addFieldnames = ["id", "url", "assetUrl", "filename", "title", "contributors", "date"]
+fieldNames = addFieldnames[:]
+rows = []
+rowLookup = None
+
+if not a.OVERWRITE and os.path.isfile(a.OUTPUT_FILE):
+    existingFieldNames, rows = readCsv(a.OUTPUT_FILE)
+    fieldNames = unionLists(addFieldnames, existingFieldNames)
+    rowLookup = createLookup(rows, "id")
 
 # Make sure output dirs exist
 makeDirectories(a.OUTPUT_FILE)
 
 def readItem(fn):
     global a
+    global rowLookup
 
     item = readJSON(fn)
+    hasExistingData = rowLookup is not None
 
     if not item or "resources" not in item or len(item["resources"]) < 1:
         return None
@@ -45,6 +57,10 @@ def readItem(fn):
     itemMeta = item["item"]
     itemUrl = itemMeta["id"]
     itemId = itemUrl.strip("/").split("/")[-1]
+
+    returnData = {}
+    if hasExistingData and itemId in rowLookup:
+        returnData = rowLookup[itemId]
 
     assetUrl = None
     for resource in item["resources"]:
@@ -68,19 +84,21 @@ def readItem(fn):
 
     fileExt = getFileExt(assetUrl)
     destFn = itemId + fileExt
-    contributor = "" if "contributor_names" not in itemMeta or len(itemMeta["contributor_names"]) < 1 else itemMeta["contributor_names"][0]
+    contributors = [] if "contributor_names" not in itemMeta or len(itemMeta["contributor_names"]) < 1 else itemMeta["contributor_names"]
+    contributors = " | ".join(contributors)
     date = "" if "date" not in itemMeta else itemMeta["date"]
 
-    return {
+    newData = {
         "id": itemId,
         "url": itemUrl,
         "assetUrl": assetUrl,
         "filename": destFn,
         "title": itemMeta["title"],
-        "contributor": contributor,
+        "contributors": contributors,
         "date": date
-        # "citationMla": item["cite_this"]["mla"]
     }
+    returnData.update(newData)
+    return returnData
 
 print("Reading metadata...")
 pool = ThreadPool(getThreadCount(a.THREADS))
@@ -95,4 +113,6 @@ print("Found %s valid items with assets" % len(rows))
 if a.PROBE:
     sys.exit()
 
-writeCsv(a.OUTPUT_FILE, rows)
+# fieldNames.remove('contributor')
+
+writeCsv(a.OUTPUT_FILE, rows, headings=fieldNames)
