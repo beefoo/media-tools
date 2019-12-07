cbc4e852fc0d19202eae79c5b1c74553bfd9ccfc
beefoo <brian@youaremyjoy.org>
2019-12-06 21:27:04 -0500

    filter csv: add probe, allow updating of input

--- filter_csv.py
+++ filter_csv.py
@@ -17,8 +17,11 @@ parser.add_argument('-in', dest="INPUT_FILE", default="tmp/samples.csv", help="I
 parser.add_argument('-filter', dest="FILTER", default="", help="Filter string")
 parser.add_argument('-sort', dest="SORT", default="", help="Sort string")
 parser.add_argument('-limit', dest="LIMIT", default=-1, type=int, help="Number of results to limit to; -1 for no limit")
-parser.add_argument('-out', dest="OUTPUT_FILE", default="tmp/samples_subset.csv", help="File to output results")
+parser.add_argument('-probe', dest="PROBE", action="store_true", help="Just show details?")
+parser.add_argument('-out', dest="OUTPUT_FILE", default="", help="File to output results; leave empty to update input file")
 a = parser.parse_args()
+
+OUTPUT_FILE = a.OUTPUT_FILE if len(a.OUTPUT_FILE) > 0 else a.INPUT_FILE
 
 # Read files
 fieldNames, rows = readCsv(a.INPUT_FILE)
@@ -39,4 +42,7 @@ if a.LIMIT > 0 and rowCount > a.LIMIT:
     rowCount = len(rows)
     print("%s rows after limiting" % rowCount)
 
-writeCsv(a.OUTPUT_FILE, rows, headings=fieldNames)
+if a.PROBE:
+    sys.exit()
+
+writeCsv(OUTPUT_FILE, rows, headings=fieldNames)
