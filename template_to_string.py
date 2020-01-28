755160219f788eb88ce2e4acf298cfe6d4f56675
Brian Foo <brian@youaremyjoy.org>
2020-01-28 16:20:10 -0500

    template to string: add support for output file

--- template_to_string.py
+++ template_to_string.py
@@ -11,6 +11,7 @@ from lib.io_utils import *
 parser = argparse.ArgumentParser()
 parser.add_argument('-in', dest="TEMPLATE_FILE", default="command_template.txt", help="Input template file")
 parser.add_argument('-query', dest="QUERY", default="key=value&key2=value2", help="Query string")
+parser.add_argument('-out', dest="OUTPUT_FILE", default="", help="Output file")
 a = parser.parse_args()
 
 fileContents = ""
@@ -19,6 +20,14 @@ with open(a.TEMPLATE_FILE, 'r') as f:
 
 params = parseQueryString(a.QUERY)
 
-print("==================\n")
-print(fileContents.format(**params))
-print("==================")
+outString = fileContents.format(**params)
+
+if len(a.OUTPUT_FILE) < 1:
+    print("==================\n")
+    print(outString)
+    print("==================")
+
+else:
+    with open(a.OUTPUT_FILE, "w") as f:
+        f.write(outString)
+    print("Wrote commands to: %s" % a.OUTPUT_FILE)
