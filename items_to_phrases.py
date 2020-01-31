167c3bc6cc92c77abeee2bb1c0835064b513c956
beefoo <brian@youaremyjoy.org>
2020-01-31 02:14:07 -0500

    items to phrases: add ovewrite param

--- items_to_phrases.py
+++ items_to_phrases.py
@@ -23,6 +23,7 @@ parser.add_argument('-dir', dest="SAMPLE_FILE_DIRECTORY", default="tmp/ia_fedfli
 parser.add_argument('-out', dest="OUTPUT_DIR", default="output/phrases/", help="Output csv file")
 parser.add_argument('-params', dest="PARAMS", default="", help="Parameters in query string format")
 parser.add_argument('-probe', dest="PROBE", action="store_true", help="Just show details?")
+parser.add_argument('-overwrite', dest="OVERWRITE", action="store_true", help="Overwrite existing data?")
 parser.add_argument('-threads', dest="THREADS", default=3, type=int, help="Number of concurrent threads, -1 for all available")
 parser.add_argument('-pyv', dest="PYTHON_NAME", default="python3", help="Name of python command")
 a = parser.parse_args()
@@ -36,6 +37,10 @@ for i, item in enumerate(items):
 
 def getItemPhrases(item):
     global a
+
+    if not a.OVERWRITE and os.path.isfile(item['phrasefilename']):
+        print("%s already exists" % item['phrasefilename'])
+        return
 
     command = [a.PYTHON_NAME, 'samples_to_phrases.py',
                '-in', item["samplefilename"],
